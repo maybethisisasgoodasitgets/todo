27c37c467e66b13d900469d6d3ba14677da8cfb0
Reza Sarijlou <u0154105@utah.edu>
2022-10-21 14:32:30 -0600

    fronend code for todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import AddBar from './components/addbar';
+import Todolist from './components/todolist';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <AddBar/>
+      <Todolist/>
+
+
     </div>
   );
 }
